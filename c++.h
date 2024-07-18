a511421bcab7b339ebb7a014be3b7c718b4149a0
Thanh16080 <thanhdeptry168@gmail.com>
2024-07-18 16:20:49 +0700

    Update and rename c.h to c++.h

---
+++ c++.h
@@ -0,0 +1,28 @@
+#include<string>
+using namespace std;
+#pragma once
+
+class Stationery {
+    int id;
+    string name;
+    double price;
+    int quantity;
+
+public:
+    Stationery(int _id = 0, string _n = " ", double _p = 0, int _q = 0);
+    int getId() const; 
+    string getName() const; 
+    double getPrice() const; 
+    int getQuantity() const;
+    void setId(int _id);
+    void setName(string name);
+    void setPrice(double price);
+    void setQuantity(int quantity);
+    void nhaptt();
+    void hienthitt() const; 
+};      
+
+
+
+
+
